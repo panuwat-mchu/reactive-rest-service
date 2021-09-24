ba1bcb8f850c448e1d058d311a4c78bb4dc9fb7d
Panuwat Muangchu <35796533+panuwat-mchu@users.noreply.github.com>
2021-09-24 14:12:15 +0700

    JWT

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -15,10 +17,12 @@ import java.util.ArrayList;
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
+	}
 
-//		GreetingClient greetingClient = context.getBean(GreetingClient.class);
-//		System.out.println(">> message = " + greetingClient.getMessage().block());
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
